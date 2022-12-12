4d0de5b66b67c7c15226c3da2edbdc4625008fad
Skanner33 <mathewmageto@gmail.com>
2022-12-12 07:48:31 +0300

    Completed core deliverables

---
+++ index.js
@@ -0,0 +1,84 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const movies = document.getElementById('films')
+    
+    const filmSetup = document.getElementById('filmSetup')
+    
+    const loadFilm = () => {
+        fetch('http://localhost:3000/films')
+        .then((response) => response.json())
+        .then((data) => createFilmDetails(data, filmSetup))
+    } 
+    loadFilm();
+
+    fetch('http://localhost:3000/films')
+    .then(resp => resp.json())
+    .then(data => {
+
+        data.forEach(films => {
+            
+            const movieList = document.createElement('li')
+
+            movieList.addEventListener('click', () => {
+                filmSetup.innerHTML = ''
+                fetch(`http://localhost:3000/films/${films.id}`)
+                .then(resp => resp.json())
+                .then(data => {
+                    createFilmDetails(data, filmSetup)
+                })
+            })
+
+            const pTitle = document.createElement('p')
+            pTitle.innerText = films.title
+            movieList.appendChild(pTitle)
+            movies.appendChild(movieList)
+        });
+    })
+})
+
+function createFilmDetails(data, andAdd){
+    const title = document.createElement('h2')
+    title.innerText = data.title
+    andAdd.appendChild(title)
+
+    const runtime = document.createElement('p')
+    runtime.innerHTML = `<b>Show Time:</b> ${data.runtime}`
+    andAdd.appendChild(runtime)
+
+    const poster = document.createElement('img')
+    poster.src = data.poster
+    andAdd.appendChild(poster)
+
+    const showtime = document.createElement('p')
+    showtime.innerHTML = `<b>Show Time:</b> ${data.showtime}<p><b>Available Tickets<b></p>`
+    andAdd.appendChild(showtime)
+
+    const description = document.createElement('p')
+    description.innerText = data.description
+    andAdd.appendChild(description)
+
+    const capacity = data.capacity
+    const soldTickets = data.tickets_sold
+    let remainingTickets = capacity - soldTickets
+    
+    const ticketsAvailable = document.createElement('p')
+    ticketsAvailable.innerText = remainingTickets
+    andAdd.appendChild(ticketsAvailable)
+
+    const buyTicketBtn = document.createElement('button')
+    buyTicketBtn.innerText = 'Buy Ticket'
+    buyTicketBtn.addEventListener('click', () => {
+        if(remainingTickets > 1){
+            remainingTickets -= 1
+            ticketsAvailable.innerText = remainingTickets
+        }else{
+            ticketsAvailable.innerText = 0
+            buyTicketBtn.disabled = true
+            buyTicketBtn.innerText = "SOLD OUT"
+        }
+    })
+    andAdd.appendChild(buyTicketBtn)
+
+
+} 
